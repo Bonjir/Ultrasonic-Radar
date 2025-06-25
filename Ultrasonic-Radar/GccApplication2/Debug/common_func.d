b950e28817f289c3b5a6e3aa908e696c74990bb6
Bonjir <2111744147@qq.com>
2025-06-25 16:13:24 +0800

    Update Readme

--- Ultrasonic-Radar/GccApplication2/Debug/common_func.d
+++
@@ -1,7 +0,0 @@
-common_func.d common_func.o: .././common_func.cpp \
- d:\program\ files\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h \
- d:\program\ files\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h
-
-d:\program\ files\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h:
-
-d:\program\ files\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
